5bd00e9ffc4e86e932f30e05d7f04d95c3aa8e93
Seungkyu-Han <trust1204@gmail.com>
2024-11-21 16:31:43 +0900

    FEAT: add project dependency each module

--- product/presentation/build.gradle.kts
+++ product/presentation/build.gradle.kts
@@ -25,6 +25,8 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":product:core"))
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
