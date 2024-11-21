f1e854276048089e32f4cd438f84d59047ffe0fb
Seungkyu-Han <trust1204@gmail.com>
2024-11-21 15:47:25 +0900

    FEAT: add product group entity

--- product/persistence/src/main/kotlin/com/seungkyu/product/entity/ProductGroup.kt
+++ product/persistence/src/main/kotlin/com/seungkyu/product/entity/ProductGroup.kt
@@ -4,11 +4,8 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document(collection = "products")
-data class Product(
+@Document(collection = "product_groups")
+data class ProductGroup(
     @Id
-    var id: ObjectId? = null,
-    var price: Int,
-
-    var productGroupId: ObjectId
+    var id: ObjectId? = null
 )
