41e84e23c5bb44de41069f6231e050f321cd4e88
Seungkyu-Han <trust1204@gmail.com>
2024-11-21 15:12:04 +0900

    first commit

---
+++ src/main/kotlin/seungkyu/ecommerce/ECommerceApplication.kt
@@ -0,0 +1,11 @@
+package seungkyu.ecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ECommerceApplication>(*args)
+}
