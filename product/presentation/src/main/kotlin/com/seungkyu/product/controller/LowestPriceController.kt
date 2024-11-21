087ea18bc6ed219ce44e460f4dc287f44d6cf14c
Seungkyu-Han <trust1204@gmail.com>
2024-11-21 17:12:30 +0900

    FEAT: add test Controller to Swagger

--- product/presentation/src/main/kotlin/com/seungkyu/product/controller/LowestPriceController.kt
+++ product/presentation/src/main/kotlin/com/seungkyu/product/controller/LowestPriceController.kt
@@ -1,12 +1,8 @@
 package com.seungkyu.product.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.headers.Header
-import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpHeaders
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
